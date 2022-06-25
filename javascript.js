206678fab205de06da213223f6a5a085db3894ea
Richard Nguyen <nguyenrichard972@gmail.com>
2022-06-25 14:20:50 -0700

    Edit javascript.js

--- javascript.js
+++ javascript.js
@@ -7,9 +7,43 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    return playerSelection == "rock" && computerSelection == "paper";
+    let res = "";
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock")
+            res = "Tie! Both are Rock";
+        else if (computerSelection == "paper")
+            res = "You Lose! Paper beats Rock";
+        else
+            res = "You win! Rock beats Scissor"
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock")
+            res = "You Win! Paper beats Rock";
+        else if (computerSelection == "paper")
+            res = "Tie! Both are Paper";
+        else
+            res = "You Lose! Scissor beats Paper"
+    } else {
+        if (computerSelection == "rock")
+            res = "You Lose! Rock beats Scissor";
+        else if (computerSelection == "paper")
+            res = "You Win! Scissor beats Paper";
+        else
+            res = "Tie! Both are Scissor";
+    }
+    return res;
 }
 
-const playerSelection = "ROCK";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let userWin = 0, compWin = 0;
+    for (let i = 0; i < 5; i++) {
+        let input = prompt("Type in rock, paper, scissor");
+        playRound(input, computerPlay());
+    }
+}
+
+game();
+
+// const playerSelection = "ROCK";
+// const computerSelection = computerPlay();
+// console.log("Computer chose: " + computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
